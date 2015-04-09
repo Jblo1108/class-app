f5f994fd91edbf5dfb07030f83b0d5f3ccd450cb
Gerard Briones <brionesgc@vcu.edu>
2015-04-08 20:42:05 -0400

    added authentication to index.js, now clicking a button will bring up the login screen

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,22 @@
+var GoogleAuth = require('googleAuth');
+var googleAuth = new GoogleAuth({
+	clientId : '809132537693-fe2pma0vrh30dcu758m4pvh9lug71e71.apps.googleusercontent.com',
+	clientSecret : 'yIUELoD1Acvw-vMYY4rsgHcs',
+	propertyName : 'googleToken',
+	quiet : false,
+	scope : [ 'https://www.googleapis.com/auth/tasks', 'https://www.googleapis.com/auth/tasks.readonly' ]
+});
+
 function doClick(e) {
     alert("You clicked a button!");
-}
+    googleAuth.isAuthorized(function() {
+        Ti.API.info('Access Token: ' + googleAuth.getAccessToken());
+        //user is authorized so do something... just dont forget to add accessToken to your requests
 
-var GoogleAuth = require('googleAuth');
+    }, function() {
+        //authorize first
+        googleAuth.authorize();
+    });
+}
 
 $.index.open();
