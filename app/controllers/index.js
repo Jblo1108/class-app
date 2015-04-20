8e34959936d0f2f22f0f5760f663a7d61ca8e064
Gerard Briones <brionesgc@vcu.edu>
2015-04-19 21:33:40 -0400

    changed settings button to weather button which displays an alert with current temperature and conditions for RVA

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,23 @@
 var googleAuth = Alloy.Globals.googleAuth;
 
+function loadWeather() {
+	var url = "http://api.wunderground.com/api/1e757fdf8c6a622c/conditions/q/VA/Richmond.json";
+	var loader = Titanium.Network.createHTTPClient({
+		onload: function(e) {
+			weatherJson = JSON.parse(this.responseText);
+			Ti.API.info(this.responseText);
+			alert("Current Temperature: " + weatherJson.current_observation.temp_f + "\nCurrent Conditions: " + weatherJson.current_observation.icon);
+		},
+		onerror: function(e) {
+			Ti.API.debug(e.error);
+			alert('error');
+		},
+		timeout:5000
+	});
+	loader.open("GET", url);
+	loader.send();
+}
+
 function login() {
     googleAuth.isAuthorized(function() {
         Ti.API.info('Access Token: ' + googleAuth.getAccessToken());
@@ -24,5 +42,4 @@ function openGrades() {
 	var grades = Alloy.createController('grades').getView();
 	grades.open();
 }
-
 $.index.open();
