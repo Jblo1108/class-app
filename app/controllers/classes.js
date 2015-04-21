5d2a92b6440104cfd1396484032a429d0a4f62b4
Gerard Briones <brionesgc@vcu.edu>
2015-04-21 13:15:26 -0400

    displaying classes to console

--- app/controllers/classes.js
+++ app/controllers/classes.js
@@ -4,7 +4,11 @@ $.classesWin.addEventListener("focus", function() {
 		    var json = this.responseText;
 		    var response = JSON.parse(json);
 		    if (response != null) {
-		    	alert('hi');
+		    	for(var i = 0; i < response.length; i++) {
+		    		response[i].forEach(function(entry) {
+		    			Ti.API.info(entry.classname);
+		    		});
+		    	}
 		    } else {
 		        alert("ERROR");
 		    }
